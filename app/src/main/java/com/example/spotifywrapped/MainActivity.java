fb7f9fd10544a1f9ca387424dd3e282389962a6b
Darnell-Chen <realdarnellchen@gmail.com>
2024-03-03 23:43:41 -0500

    need to set up Register Activity

--- app/src/main/java/com/example/spotifywrapped/MainActivity.java
+++ app/src/main/java/com/example/spotifywrapped/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.spotifywrapped;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,19 +14,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button loginButton;
+    private TextView registerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login_page);
 
-        Button loginButton = findViewById(R.id.loginButton);
-        TextView registerButton = findViewById(R.id.registerButton);
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.registerButton);
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // need new intent to open a register page
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                finish();
             }
         });
 
