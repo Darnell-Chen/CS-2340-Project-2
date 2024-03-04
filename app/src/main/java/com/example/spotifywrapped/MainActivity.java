a3688928c1cc4c8866661c1f156d0ac324791d70
Darnell-Chen <realdarnellchen@gmail.com>
2024-03-04 02:00:49 -0500

    completed registration and login

--- app/src/main/java/com/example/spotifywrapped/MainActivity.java
+++ app/src/main/java/com/example/spotifywrapped/MainActivity.java
@@ -2,27 +2,46 @@ package com.example.spotifywrapped;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText email;
+    private EditText password;
     private Button loginButton;
     private TextView registerButton;
+
+    private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.login_page);
 
+        email = findViewById(R.id.loginEmail);
+        password = findViewById(R.id.loginPassword);
         loginButton = findViewById(R.id.loginButton);
         registerButton = findViewById(R.id.registerButton);
 
+        auth = FirebaseAuth.getInstance();
+
+        // opens up the Registration Page
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,6 +50,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // logins the user if the input is successfully authenticated
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputEmail = email.getText().toString();
+                String inputPassword = password.getText().toString();
 
+                if (TextUtils.isEmpty(inputEmail) || TextUtils.isEmpty(inputPassword)) {
+                    Toast.makeText(MainActivity.this, "please input login info", Toast.LENGTH_SHORT).show();
+                } else {
+                    loginUser(inputEmail, inputPassword);
+                }
+            }
+        });
+
+
+    }
+
+    private void loginUser(String pEmail, String pPass) {
+        auth.signInWithEmailAndPassword(pEmail, pPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "login successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "login failed. try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/spotifywrapped/RegisterActivity.java
+++ app/src/main/java/com/example/spotifywrapped/RegisterActivity.java
@@ -6,6 +6,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,6 +22,7 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText email;
     private EditText password;
     private Button registerButton;
+    private TextView backButton;
 
     private FirebaseAuth auth;
 
@@ -30,8 +32,9 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.register_page);
 
         email = findViewById(R.id.registerEmail);
-        password = findViewById(R.id.registerEmail);
+        password = findViewById(R.id.registerPassword);
         registerButton = findViewById(R.id.registerButton2);
+        backButton = findViewById(R.id.registerBack);
 
         auth = FirebaseAuth.getInstance();
 
@@ -41,12 +44,26 @@ public class RegisterActivity extends AppCompatActivity {
                 String newEmail = email.getText().toString();
                 String newPassword = password.getText().toString();
 
+
                 // Android Studio prefers Text.Utils according to Stack Overflow
                 if (TextUtils.isEmpty(newEmail) || TextUtils.isEmpty(newPassword)) {
                     Toast.makeText(RegisterActivity.this, "Please input credentials", Toast.LENGTH_SHORT).show();
+
+                } else if (newPassword.length() < 6) {
+                    // Note that this requirement is by Firebase - not our choice
+                    Toast.makeText(RegisterActivity.this, "Password has to be at least 6 characters long", Toast.LENGTH_SHORT).show();
+
                 } else {
                     register(newEmail, newPassword);
                 }
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                finish();
             }
         });
     }
