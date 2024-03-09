3b1dd757d64ec6c1eabacf28a45b6b6a5ac067a4
Darnell-Chen <realdarnellchen@gmail.com>
2024-03-09 02:07:44 -0500

    made gradient background, working on fragments

--- app/src/main/java/com/example/spotifywrapped/WrappedActivity.java
+++ app/src/main/java/com/example/spotifywrapped/WrappedActivity.java
@@ -1,8 +1,10 @@
 package com.example.spotifywrapped;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class WrappedActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class WrappedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wrapped_layout);
+
+        ConstraintLayout background = findViewById(R.id.wrappedBackground);
+
+        AnimationDrawable animDrawable = (AnimationDrawable) background.getBackground();
+        animDrawable.setEnterFadeDuration(2500);
+        animDrawable.setExitFadeDuration(3000);
+        animDrawable.start();
     }
 }
