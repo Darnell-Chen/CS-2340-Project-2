064e479777bbd06b26e2695524083b0dc15b6a41
Tarunkrishna Ramji <tramji3@gatech.edu>
2024-03-13 15:38:16 -0400

    llm basic fragment/animation

--- app/src/main/java/com/example/spotifywrapped/SummaryFragment.java
+++ app/src/main/java/com/example/spotifywrapped/SummaryFragment.java
@@ -1,12 +1,15 @@
 package com.example.spotifywrapped;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +63,15 @@ public class SummaryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_summary, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        FrameLayout background = getView().findViewById(R.id.summaryBackground);
+
+        AnimationDrawable animDrawable = (AnimationDrawable) background.getBackground();
+        animDrawable.setEnterFadeDuration(2000);
+        animDrawable.setExitFadeDuration(2500);
+        animDrawable.start();
     }
 }
