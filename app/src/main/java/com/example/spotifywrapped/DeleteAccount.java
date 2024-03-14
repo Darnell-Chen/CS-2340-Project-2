fd351c1ab49891b64a4db9751ecfcd21eed53888
Syed Adil Husain <adil.husain05@outlook.com>
2024-03-14 12:28:17 -0400

    PasswrodConfirmation

--- app/src/main/java/com/example/spotifywrapped/DeleteAccount.java
+++ app/src/main/java/com/example/spotifywrapped/DeleteAccount.java
@@ -8,11 +8,17 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class DeleteAccount {
+public class DeleteAccount extends AppCompatActivity {
 
     private final Context context;
 
@@ -57,5 +63,25 @@ public class DeleteAccount {
     private void logout() {
         Intent loginPage = new Intent (context, MainActivity.class);
         context.startActivity(loginPage);
+    }
+
+    private void showDeleteConfirmationDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(DeleteAccount.this);
+        LayoutInflater inflater = LayoutInflater.from(DeleteAccount.this);
+        View passwordView = inflater.inflate(R.layout.password_confirmation, null);
+        builder.setView(passwordView);
+
+        final EditText passwordEditText = passwordView.findViewById(R.id.passwordEditText);
+        Button confirmButton = passwordView.findViewById(R.id.confirmButton);
+
+        AlertDialog dialog = builder.create();
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String password = passwordEditText.getText().toString();
+                // TODO: Implement the logic after password is entered and CONFIRM is clicked
+                dialog.dismiss();
+            }
+        });
     }
 }
