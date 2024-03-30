706f9e11243d488a8ce042cdc0cff1a8d37847ce
Darnell-Chen <realdarnellchen@gmail.com>
2024-03-30 16:14:12 -0400

    added reAuth to account deletion - now needs to merge w/ Adil to get pop-up

--- app/src/main/java/com/example/spotifywrapped/DeleteAccount.java
+++ app/src/main/java/com/example/spotifywrapped/DeleteAccount.java
@@ -7,8 +7,14 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -31,13 +37,7 @@ public class DeleteAccount {
 
             public void onClick(DialogInterface dialog, int which) {
 
-                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-                currentUser.delete();
-
-                Toast.makeText(context, "Account has been deleted", Toast.LENGTH_SHORT).show();
-
-                dialog.dismiss();
-                logout();
+                reAuth();
             }
         });
 
@@ -52,6 +52,31 @@ public class DeleteAccount {
 
         AlertDialog alert = builder.create();
         alert.show();
+    }
+
+    private void reAuth() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        // Get auth credentials from the user for re-authentication. The example below shows
+        // email and password credentials but there are multiple possible providers,
+        // such as GoogleAuthProvider or FacebookAuthProvider.
+        AuthCredential credential = EmailAuthProvider
+                .getCredential("user@example.com", "password1234");
+
+        // Prompt the user to re-provide their sign-in credentials
+        user.reauthenticate(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                        currentUser.delete();
+
+                        Toast.makeText(context, "Account has been deleted", Toast.LENGTH_SHORT).show();
+
+//                        dialog.dismiss();
+                        logout();
+                    }
+                });
     }
 
     private void logout() {
