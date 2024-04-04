9e63793a4f5d16fa6443f15638ebd99fe6e4a076
MameBabou <mbabou3@gatech.edu>
2024-04-04 14:02:47 -0400

    attempting to pull profile image

--- app/src/main/java/com/example/spotifywrapped/DashboardActivity.java
+++ app/src/main/java/com/example/spotifywrapped/DashboardActivity.java
@@ -6,19 +6,33 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    private ImageView profileImg;
+    private DatabaseReference currReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard_page);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        profileImg = findViewById(R.id.imageView);
+        currReference = FirebaseDatabase.getInstance().getReference();
+
+        //Users id
+        DatabaseReference profileRef = currReference.child("Users").child(auth.getUid().toString()).child("profile");
+
+
     }
 
     public void openWrapped(View view) {
